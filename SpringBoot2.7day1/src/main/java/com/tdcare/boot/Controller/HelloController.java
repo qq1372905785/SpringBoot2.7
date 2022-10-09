86a31d05a86494e38df49f356fc7e4fe422c9e8a
ljhGitHub <1372905675@qq.com>
2022-10-10 00:52:44 +0800

    这是一个相信项目

---
+++ SpringBoot2.7day1/src/main/java/com/tdcare/boot/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.tdcare.boot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller     //用注解声明这是一个Controller类
+//@ResponseBody   //这个类的方法都可以返回字符串
+@RestController    //RestController是responseBody和Controller的结合体
+public class HelloController {
+    @ResponseBody       //因为返回的是字符串，所以用@ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,SpringBoot111";
+    }
+}
