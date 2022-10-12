cc74b21959b2cd160d833ae716803c5fd75ac4c9
ljhGitHub <1372905675@qq.com>
2022-10-12 14:00:48 +0800

    修改了包名

--- SpringBoot2.7day1/src/main/java/com/tdcare/boot/controller/HelloController.java
+++ SpringBoot2.7day1/src/main/java/com/tdcare/boot/controller/HelloController.java
@@ -1,6 +1,5 @@
-package com.tdcare.boot.Controller;
+package com.tdcare.boot.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
