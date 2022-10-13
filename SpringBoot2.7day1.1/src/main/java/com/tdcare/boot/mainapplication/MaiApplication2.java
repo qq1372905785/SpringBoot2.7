52d3168b599b65a786a0a4fd2cafa83e4cf0838c
ljhGitHub <1372905675@qq.com>
2022-10-13 22:27:48 +0800

    修改了注释

--- SpringBoot2.7day1.1/src/main/java/com/tdcare/boot/mainapplication/MaiApplication2.java
+++ SpringBoot2.7day1.1/src/main/java/com/tdcare/boot/mainapplication/MaiApplication2.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 * 包扫描主要是扫描controller的包的注解，在默认情况下是扫描主程序同层级和主程序下的子包
  * 如果修改了主程序或者controller的包的位置，则需要用自定义扫描
 * */
-//@SpringBootApplication(scanBasePackages = "com.tdcare.boot.controller")
+//@SpringBootApplication(scanBasePackages = "com.tdcare.boot.controller")   自定义扫描
 /*@SpringBootApplication
-@ComponentScan("com.tdcare.boot.controller")*/
+@ComponentScan("com.tdcare.boot.controller")自定义扫描*/
 public class MaiApplication2 {
     public static void main(String[] args) {
         SpringApplication.run(MaiApplication2.class,args);
